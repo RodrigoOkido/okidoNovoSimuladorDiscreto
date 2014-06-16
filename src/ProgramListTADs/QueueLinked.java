ca83c00120ef4c343e5937bbc001eecf80c89792
Rodrigo Okido <rodrigo_okido@hotmail.com>
2014-06-16 15:01:40 -0300

    versão 1.0 build 160614

--- src/ProgramListTADs/QueueLinked.java
+++ src/ProgramListTADs/QueueLinked.java
@@ -3,13 +3,50 @@ package ProgramListTADs;
 import ProgramExceptions.EmptyQueueException;
 import ProgramInterfaces.QueueTAD;
 
+/**
+ * Classe responsável pela criação de uma fila. A classe implementa uma interface genérica 
+ * chamada "QueueTAD" e possui todos os métodos necessários para o funcionamento de uma fila
+ * de qualquer tipo (genéricos). A classe trabalha de forma duplamente encadeada, podendo trabalhar
+ * nela pelos dois lados. 
+ * 
+ * @author Rodrigo Okido
+ * @version 1.0   
+ */
 public class QueueLinked<E> implements QueueTAD<E>
 {
+	
+	/*
+	 * Classe interna que corresponde a criação dos nodos da fila. Cada nodo pode guardar
+	 * um elemento de algum tipo genérico dentro dele, e referenciar o próximo elemento dele e 
+	 * o anterior.
+	 */
     private static final class Node<E> {
+    	
+    	/*
+    	 * Atributo que guarda um elemento de um tipo qualquer dentro do nodo.
+    	 */
         public E element;
+        
+        
+        /*
+         * Atributo que referencia o próximo nodo de algum elemento.
+         */
         public Node<E> next;
+        
+        
+        /*
+         * Atributo que referencia o nodo anterior de algum elemento. 
+         */
         public Node<E> prev;
         
+        
+        /*
+         * Construtor para a criação do Nodo. Recebe um elemento por parâmetro tendo 
+         * o próximo e o anterior ao nodo criado inicializados com null.
+         * 
+         * @param e Recebe por parâmetro um elemento de tipo genérico para ser colocado
+         * dentro do nodo.
+         */
         public Node(E e){
             element = e;
             prev = null;
@@ -18,27 +55,65 @@ public class QueueLinked<E> implements QueueTAD<E>
         }
     }
 
+    
+    /*
+     * Atributo que corresponde ao Nodo inicial de uma lista qualquer. Por ser uma fila, 
+     * o primeiro elemento que entrar na fila, deve ser o primeiro elemento a sair.  
+     */
     private Node<E> head;
+    
+    
+    /*
+     * Atributo que corresponde ao Nodo Final de uma lista qualquer. Por ser uma fila, 
+     * o último elemento que entrar na fila, deve ser o último elemento a sair.
+     */
     private Node<E> tail;
+    
+    
+    /*
+     * Atributo para controlar o tamanho atual da fila.
+     */
     private int count;
     
+    
+    /*
+     * Construtor de uma fila. Iniciada com todos os seus atributos vazios, ou seja, "head" 
+     * e "tail" com null, e count com zero elementos.
+     */
     public QueueLinked()
     {
         head = null;
         tail = null;
         count = 0;        
     }
-
+    
+    
+    /*
+     * Verifica o tamanho atual da fila.
+     * 
+     * @return retorna o tamanho atual da fila
+     */
     public int size()
     {
         return count;
     }
 
+    
+    /*
+     * Verifica se a fila está vazia ou não.
+     * 
+     * @return retorna "true" se a lista se encontra vazia, e "false" caso contrário
+     */
     public boolean isEmpty()
     {
         return (count == 0);
     }
     
+    
+    /*
+     * Método responsável por limpar toda a fila. Seus elementos serão todos excluídos 
+     * e count zerado.
+     */
     public void clear()
     {
         head = null;
@@ -46,13 +121,27 @@ public class QueueLinked<E> implements QueueTAD<E>
         count = 0;        
     }
 
+    
+    /*
+     * Retorna o elemento que se encontra em primeiro na fila.
+     * 
+     * @throws joga uma EmptyQueueException caso a fila estiver vazia
+     * @return retorna o primeiro elemento da fila
+     */
     public E element()
     {
         if(isEmpty())
             throw new EmptyQueueException();
         return head.element;
     }
+  
     
+  /*
+   * Adiciona um elemento de um tipo qualquer dentro de uma fila.
+   * 
+   * @param element Adiciona na fila por parâmetro um elemento de um tipo genérico,
+   * podendo ser de qualquer tipo.
+   */
     public void add(E element)
     {
         Node<E> n = new Node<E>(element);
@@ -66,6 +155,7 @@ public class QueueLinked<E> implements QueueTAD<E>
         tail = n;
         count++;        
     }
+    
     
     /*
      * Remove da lista a partir da "Head", isto é, o primeiro elemento ao ser adicionado,
@@ -88,6 +178,14 @@ public class QueueLinked<E> implements QueueTAD<E>
         return item;            
     }
 
+    
+    /*
+     * Remove da lista a partir da "Tail", isto é, o último elemento ao ser adicionado,
+     * ele será o primeiro a ser removido. Tem um funcionamento similar ao uma pilha neste caso,
+     * por isso, deve ser usado apenas caso necessário.  
+     * 
+     * @return retorna o elemento que foi retirado
+     */
 	public E removeFromTail() {
 		if(isEmpty())
             throw new EmptyQueueException();	
@@ -99,8 +197,7 @@ public class QueueLinked<E> implements QueueTAD<E>
 		if (tail == null)
 			head = null;
 		count --;
-		return item;
-		
+		return item;	
 }    
 	
 }
--- src/ProgramListTADs/StackLinked.java
+++ src/ProgramListTADs/StackLinked.java
@@ -3,13 +3,49 @@ package ProgramListTADs;
 import ProgramExceptions.EmptyStackException;
 import ProgramInterfaces.StackTAD;
 
-public class StackLinked <E> implements StackTAD<E>{
+/**
+ * Classe responsável pela criação de uma pilha. A classe implementa uma interface genérica 
+ * chamada "StackTAD" e possui todos os métodos necessários para o funcionamento de uma pilha
+ * de qualquer tipo (genéricos). 
+ * 
+ * @author Rodrigo Okido
+ * @version 1.0   
+ */
+public class StackLinked <E> implements StackTAD<E>
+{
 	
+	/*
+	 * Classe interna que corresponde a criação dos nodos da fila. Cada nodo pode guardar
+	 * um elemento de algum tipo genérico dentro dele, e referenciar o próximo elemento dele e 
+	 * o anterior. 
+	 */
 	 private static final class Node<E> {
+		 
+		    /*  
+			 * Atributo que guarda um elemento de um tipo qualquer dentro do nodo.
+	    	 */
 	        public E element;
+	        
+	        
+	        /*
+	         * Atributo que referencia o próximo nodo de algum elemento.
+	         */
 	        public Node<E> next;
+	        
+	        
+	        /*
+	         * Atributo que referencia o nodo anterior de algum elemento. 
+	         */
 	        public Node<E> prev;
 	        
+	        
+	        /*
+	         * Construtor para a criação do Nodo. Recebe um elemento por parâmetro tendo 
+	         * o próximo e o anterior ao nodo criado inicializados com null.
+	         * 
+	         * @param e Recebe por parâmetro um elemento de tipo genérico para ser colocado
+	         * dentro do nodo.
+	         */
 	        public Node(E e){
 	            element = e;
 	            next = null;
@@ -17,10 +53,31 @@ public class StackLinked <E> implements StackTAD<E>{
 	        }
 	    }
 
+	    
+	    /*
+	     * Atributo que corresponde ao Nodo inicial de uma lista qualquer. Por ser uma pilha, 
+	     * o primeiro elemento que entrar na pilha, deve ser o último elemento a sair.  
+	     */
 	    private Node<E> head;
+	    
+	    
+	    /*
+	     * Atributo que corresponde ao Nodo Final de uma lista qualquer. Por ser uma pilha, 
+	     * o último elemento que entrar na pilha, deve ser o primeiro elemento a sair.
+	     */
 	    private Node<E> tail;
+	    
+	    
+	    /*
+	     * Atributo para controlar o tamanho atual da pilha.
+	     */
 	    private int count;
 	    
+	    
+	    /*
+	     * Construtor para a criação de uma pilha. Iniciada com todos os seus atributos vazios, 
+	     * ou seja, "head" e "tail" com null, e "count" com zero elementos.
+	     */
 	    public StackLinked()
 	    {
 	        head = null;
@@ -29,7 +86,11 @@ public class StackLinked <E> implements StackTAD<E>{
 	    }
 
 
-	@Override
+	/*
+	 * Adiciona um elemento dentro da pilha.
+	 * 
+	 * @param element Adiciona por parâmetro um elemento de tipo genérico dentro da pilha
+	 */
 	public void push(E element) {
 		Node<E> newNode = new Node<E>(element);
 		if (count == 0){
@@ -47,7 +108,13 @@ public class StackLinked <E> implements StackTAD<E>{
 		
 	}
 
-	@Override
+	
+	/*
+	 * Método que remove o elemento do topo da pilha. 
+	 * 
+	 * @throws joga uma EmptyStackException se a pilha estiver vazia
+	 * @return retorna o elemento removido
+	 */
 	public E pop() {
 		if (count == 0){
 			throw new EmptyStackException ();
@@ -70,12 +137,15 @@ public class StackLinked <E> implements StackTAD<E>{
 		return guard;
 		
 		}
-		// TODO Auto-generated method stub retorna e remove o item no topo da pilha
-
-	    //Deve lançar uma exceção java.util.EmptyStackException se a pilha estiver vazia
 	}
 
-	@Override
+	
+	/*
+	 * Verifica o elemento que se encontra no topo da pilha.
+	 * 
+	 * @throws joga uma EmptyStackException se a pilha estiver vazia
+	 * @return retorna o elemento que se encontra no topo da pilha
+	 */
 	public E top() {
 		if (count == 0){
 			throw new EmptyStackException ();
@@ -86,17 +156,31 @@ public class StackLinked <E> implements StackTAD<E>{
 		}
 	}
 
-	@Override
+	
+    /*
+     * Verifica o tamanho atual da fila.
+     * 
+     * @return retorna o tamanho atual da fila
+     */
 	public int size() {
 		return count;
 	}
 
-	@Override
+	
+    /*
+     * Verifica se a fila está vazia ou não.
+     * 
+     * @return retorna "true" se a lista se encontra vazia, e "false" caso contrário
+     */
 	public boolean isEmpty() {
       	return count == 0;
 	}
 
-	@Override
+	
+    /*
+     * Método responsável por limpar toda a pilha. Seus elementos serão todos excluídos 
+     * e count zerado.
+     */
 	public void clear() {
 		head = null;
 		tail = null;
